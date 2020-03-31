cc050e30636f5267f48aadbe46596ad0a480a340
bayan <bayana.mhmd99@gmail.com>
2020-03-31 21:32:00 +0300

    not yet

--- app.js
+++ app.js
@@ -1,59 +1,61 @@
-/* eslint-disable no-undef */
-'use strict';
+// const cors = require("cors");
+// const express = require("express");
+// const stripe = require("stripe")("sk_test_AbMnCwDx29ugT0KlXkKVuWQ800IilbAsdL");
+// const uuid = require("uuid/v4");
 
-const express = require('express');
-const stripe = require('stripe')('sk_test_fmjNzHK4pMb4MyABHiAQgxXe001I54PDuG');
-const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
+// const app = express();
 
-const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-///handelbars middleware
-//configration obj
-app.engine('handlebars', exphbs({defaultLayout:'main',}));
-app.set('view engine','handlebars');
+// app.get("/", (req, res) => {
+//   res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
+// });
 
-//body parseer middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false,}));
+// app.post("/checkout", async (req, res) => {
+//   console.log("Request:", req.body);
 
+//   let error;
+//   let status;
+//   try {
+//     const { product, token } = req.body;
 
-///set static folder
+//     const customer = await stripe.customers.create({
+//       email: token.email,
+//       source: token.id
+//     });
 
-app.use(express.static(`${__dirname}/public`));
+//     const idempotency_key = uuid();
+//     const charge = await stripe.charges.create(
+//       {
+//         amount: product.price * 100,
+//         currency: "usd",
+//         customer: customer.id,
+//         receipt_email: token.email,
+//         description: `Purchased the ${product.name}`,
+//         shipping: {
+//           name: token.card.name,
+//           address: {
+//             line1: token.card.address_line1,
+//             line2: token.card.address_line2,
+//             city: token.card.address_city,
+//             country: token.card.address_country,
+//             postal_code: token.card.address_zip
+//           }
+//         }
+//       },
+//       {
+//         idempotency_key
+//       }
+//     );
+//     console.log("Charge:", { charge });
+//     status = "success";
+//   } catch (error) {
+//     console.error("Error:", error);
+//     status = "failure";
+//   }
 
-// index route
-app.get('/', (req, res) => {
-  res.render('index');
-});
+//   res.json({ error, status });
+// });
 
-app.get('/success', (req, res) => {
-  // console.log('uujujujuj', req.body);
-
-  res.render('success');
-});
-
-//charge route 
-
-app.post('/charge', (req, res) => {
-  const amount = 35000;
-  
-  stripe.customers.create({
-    email: req.body.stripeEmail,
-    source: req.body.stripeToken,
-  })
-  .then(customer => stripe.charges.create({
-    amount,
-    description: 'Car-rentals app',
-    currency: 'usd',
-    customer: customer.id,
-  }))
-  .then(charge => res.render('success'));
-});
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`server started on port ${port}`);
-
-});
+// app.listen(5000);
